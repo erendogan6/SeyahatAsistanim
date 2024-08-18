bc17799afb961a4b8e2f22f4c937077f9f3c198a
erendogan6 <erendogan6@hotmail.com>
2024-08-19 01:58:48 +0300

    feat(api): update ChatGptService to a functional interface and remove token header

--- app/src/main/java/com/erendogan6/seyahatasistanim/data/remote/ChatGptApiService.kt
+++ app/src/main/java/com/erendogan6/seyahatasistanim/data/remote/ChatGptApiService.kt
@@ -3,13 +3,11 @@ package com.erendogan6.seyahatasistanim.data.remote
 import com.erendogan6.seyahatasistanim.data.model.chatGPT.ChatGptRequest
 import com.erendogan6.seyahatasistanim.data.model.chatGPT.ChatGptResponse
 import retrofit2.http.Body
-import retrofit2.http.Header
 import retrofit2.http.POST
 
-interface ChatGptService {
+fun interface ChatGptApiService {
     @POST("v1/chat/completions")
     suspend fun getSuggestions(
-        @Header("Authorization") token: String,
         @Body request: ChatGptRequest,
     ): ChatGptResponse
 }
