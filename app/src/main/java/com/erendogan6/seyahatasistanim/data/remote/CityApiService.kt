3098ea7ccc312d387088128f4c804f33709b71b5
erendogan6 <erendogan6@hotmail.com>
2024-08-18 22:43:59 +0300

    feat(api): add CityApiService for retrieving city data by name

---
+++ app/src/main/java/com/erendogan6/seyahatasistanim/data/remote/CityApiService.kt
@@ -0,0 +1,14 @@
+package com.erendogan6.seyahatasistanim.data.remote
+
+import com.erendogan6.seyahatasistanim.data.model.weather.City
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface CityApiService {
+    @GET("geo/1.0/direct")
+    suspend fun getCities(
+        @Query("q") query: String,
+        @Query("appid") apiKey: String,
+        @Query("limit") limit: Int = 5,
+    ): List<City>
+}
