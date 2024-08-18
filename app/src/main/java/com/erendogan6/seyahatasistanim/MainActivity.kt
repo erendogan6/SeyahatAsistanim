746d243cc3da59f0f991f95d8cd5464348008292
erendogan6 <erendogan6@hotmail.com>
2024-08-18 17:11:26 +0300

    chore(project): initialize Android project with default settings

---
+++ app/src/main/java/com/erendogan6/seyahatasistanim/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.erendogan6.seyahatasistanim
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.erendogan6.seyahatasistanim.ui.theme.SeyahatAsistanımTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            SeyahatAsistanımTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding),
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(
+    name: String,
+    modifier: Modifier = Modifier,
+) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    SeyahatAsistanımTheme {
+        Greeting("Android")
+    }
+}
