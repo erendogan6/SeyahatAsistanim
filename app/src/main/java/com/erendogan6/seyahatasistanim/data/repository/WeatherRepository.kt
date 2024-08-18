bed600aee2a30d3ad4353bdec29cbb972fb01d5f
erendogan6 <erendogan6@hotmail.com>
2024-08-18 22:47:32 +0300

    feat(repository): create WeatherRepository to handle weather data fetching

---
+++ app/src/main/java/com/erendogan6/seyahatasistanim/data/repository/WeatherRepository.kt
@@ -0,0 +1,20 @@
+package com.erendogan6.seyahatasistanim.data.repository
+
+import com.erendogan6.seyahatasistanim.data.model.weather.WeatherApiResponse
+import com.erendogan6.seyahatasistanim.data.remote.WeatherApiService
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class WeatherRepository(
+    private val weatherApiService: WeatherApiService,
+) {
+    fun getWeatherForecast(
+        lat: Double,
+        lon: Double,
+        apiKey: String,
+    ): Flow<WeatherApiResponse> =
+        flow {
+            val response = weatherApiService.getWeatherForecast(lat, lon, apiKey)
+            emit(response)
+        }
+}
