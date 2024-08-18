2b6874d7c01fa04181fc2490a4e6d8005b3928a5
erendogan6 <erendogan6@hotmail.com>
2024-08-18 22:46:41 +0300

    feat(app): initialize Koin in SeyahatAsistaniApp with network module

---
+++ app/src/main/java/com/erendogan6/seyahatasistanim/di/SeyahatAsistaniApp.kt
@@ -0,0 +1,15 @@
+package com.erendogan6.seyahatasistanim.di
+
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class SeyahatAsistaniApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@SeyahatAsistaniApp)
+            modules(listOf(networkModule))
+        }
+    }
+}
