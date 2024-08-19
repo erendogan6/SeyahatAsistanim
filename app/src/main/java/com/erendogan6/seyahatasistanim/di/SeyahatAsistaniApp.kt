2fe6a5d568014b0be5e872ef4e599d1a3e70c825
erendogan6 <erendogan6@hotmail.com>
2024-08-19 21:18:30 +0300

    chore(di): add database module to Koin startKoin configuration

--- app/src/main/java/com/erendogan6/seyahatasistanim/di/SeyahatAsistaniApp.kt
+++ app/src/main/java/com/erendogan6/seyahatasistanim/di/SeyahatAsistaniApp.kt
@@ -13,6 +13,7 @@ class SeyahatAsistaniApp : Application() {
                 listOf(
                     networkModule,
                     viewModelModule,
+                    databaseModule,
                 ),
             )
         }
