abf6923091cf08838c7307f58a81abe1d9b021aa
erendogan6 <erendogan6@hotmail.com>
2024-08-21 00:35:09 +0300

    feat(database): create WeatherEntity for weather data storage

---
+++ app/src/main/java/com/erendogan6/seyahatasistanim/data/model/weather/WeatherEntity.kt
@@ -0,0 +1,16 @@
+package com.erendogan6.seyahatasistanim.data.model.weather
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.LocalDate
+
+@Entity(tableName = "weather_data")
+data class WeatherEntity(
+    @PrimaryKey val id: String,
+    val date: LocalDate,
+    val temperatureDay: Double,
+    val temperatureNight: Double,
+    val description: String,
+    val latitude: Double,
+    val longitude: Double,
+)
