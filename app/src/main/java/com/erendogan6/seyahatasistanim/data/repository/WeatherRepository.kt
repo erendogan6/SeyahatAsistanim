81cec7d5da6490447ad05ffb98a36101f8eff605
erendogan6 <erendogan6@hotmail.com>
2024-08-21 00:38:33 +0300

    feat(repository): add methods to save and retrieve weather data in WeatherRepository

--- app/src/main/java/com/erendogan6/seyahatasistanim/data/repository/WeatherRepository.kt
+++ app/src/main/java/com/erendogan6/seyahatasistanim/data/repository/WeatherRepository.kt
@@ -1,12 +1,16 @@
 package com.erendogan6.seyahatasistanim.data.repository
 
+import com.erendogan6.seyahatasistanim.data.local.WeatherDao
 import com.erendogan6.seyahatasistanim.data.model.weather.WeatherApiResponse
+import com.erendogan6.seyahatasistanim.data.model.weather.WeatherEntity
 import com.erendogan6.seyahatasistanim.data.remote.WeatherApiService
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import java.time.LocalDate
 
 class WeatherRepository(
     private val weatherApiService: WeatherApiService,
+    private val weatherDao: WeatherDao,
 ) {
     fun getWeatherForecast(
         lat: Double,
@@ -16,4 +20,13 @@ class WeatherRepository(
             val response = weatherApiService.getWeatherForecast(lat, lon)
             emit(response)
         }
+
+    suspend fun saveWeatherData(weatherEntities: List<WeatherEntity>) {
+        weatherDao.insertWeatherData(weatherEntities)
+    }
+
+    suspend fun getWeatherData(travelDate: LocalDate): List<WeatherEntity> {
+        val startDate = travelDate.minusDays(1)
+        return weatherDao.getWeatherDataForRange(startDate, travelDate)
+    }
 }
