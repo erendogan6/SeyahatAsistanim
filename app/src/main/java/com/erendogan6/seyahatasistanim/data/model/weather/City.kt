908e36269b6f7dd378cd17da3931e6c243b05e48
erendogan6 <erendogan6@hotmail.com>
2024-08-21 16:07:39 +0300

    feat(weather-model): update City model structure and add LocalNames support

--- app/src/main/java/com/erendogan6/seyahatasistanim/data/model/weather/City.kt
+++ app/src/main/java/com/erendogan6/seyahatasistanim/data/model/weather/City.kt
@@ -4,6 +4,8 @@ import com.google.gson.annotations.SerializedName
 
 data class City(
     @SerializedName("name") val name: String,
-    @SerializedName("coord") val coord: Coord,
+    @SerializedName("lat") val latitude: Double,
+    @SerializedName("lon") val longitude: Double,
     @SerializedName("country") val country: String,
+    @SerializedName("local_names") val localNames: LocalNames? = null,
 )
--- app/src/main/java/com/erendogan6/seyahatasistanim/data/model/weather/Coord.kt
+++
@@ -1,8 +0,0 @@
-package com.erendogan6.seyahatasistanim.data.model.weather
-
-import com.google.gson.annotations.SerializedName
-
-data class Coord(
-    @SerializedName("lat") val latitude: Double,
-    @SerializedName("lon") val longitude: Double,
-)
