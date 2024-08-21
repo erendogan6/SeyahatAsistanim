437eb8f3e545f5e1d06ee96b48082f4b4478d4f9
erendogan6 <erendogan6@hotmail.com>
2024-08-21 22:42:39 +0300

    feat(repository): add delete and toggle completion functions in ChecklistRepository
    - Added `deleteChecklistItem` to remove a checklist item by its ID.
    - Added `toggleChecklistItemCompletion` to switch the completion status of a checklist item.

--- app/src/main/java/com/erendogan6/seyahatasistanim/data/repository/ChecklistRepository.kt
+++ app/src/main/java/com/erendogan6/seyahatasistanim/data/repository/ChecklistRepository.kt
@@ -11,4 +11,14 @@ class ChecklistRepository(
     }
 
     suspend fun getAllChecklistItems(): List<ChecklistItemEntity> = checklistDao.getAllChecklistItems()
+
+    suspend fun deleteChecklistItem(id: Int) {
+        checklistDao.deleteChecklistItem(id)
+    }
+
+    suspend fun toggleChecklistItemCompletion(id: Int) {
+        val item = checklistDao.getChecklistItemById(id)
+        val updatedItem = item.copy(isCompleted = !item.isCompleted)
+        checklistDao.updateChecklistItem(updatedItem)
+    }
 }
