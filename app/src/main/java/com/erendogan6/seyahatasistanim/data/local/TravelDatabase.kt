0ca6e16300604c31e920fc670c1ff7cc4098555f
erendogan6 <erendogan6@hotmail.com>
2024-08-21 19:13:53 +0300

    feat(database): add LocalInfoEntity and LocalInfoDao to TravelDatabase

--- app/src/main/java/com/erendogan6/seyahatasistanim/data/local/TravelDatabase.kt
+++ app/src/main/java/com/erendogan6/seyahatasistanim/data/local/TravelDatabase.kt
@@ -3,14 +3,17 @@ package com.erendogan6.seyahatasistanim.data.local
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.erendogan6.seyahatasistanim.data.model.chatGPT.LocalInfoEntity
 import com.erendogan6.seyahatasistanim.data.model.travel.TravelEntity
 import com.erendogan6.seyahatasistanim.data.model.weather.WeatherEntity
 import com.erendogan6.seyahatasistanim.utils.Converters
 
-@Database(entities = [TravelEntity::class, WeatherEntity::class], version = 1, exportSchema = false)
+@Database(entities = [TravelEntity::class, WeatherEntity::class, LocalInfoEntity::class], version = 1, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class TravelDatabase : RoomDatabase() {
     abstract fun travelDao(): TravelDao
 
     abstract fun weatherDao(): WeatherDao
+
+    abstract fun localInfoDao(): LocalInfoDao
 }
