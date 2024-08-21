7a0b6ff79a0b8ae323834553dd85667bfeff74a4
erendogan6 <erendogan6@hotmail.com>
2024-08-21 16:14:31 +0300

    feat(repository): integrate CityApiService for city suggestions in TravelRepository

--- app/src/main/java/com/erendogan6/seyahatasistanim/data/repository/TravelRepository.kt
+++ app/src/main/java/com/erendogan6/seyahatasistanim/data/repository/TravelRepository.kt
@@ -2,17 +2,25 @@ package com.erendogan6.seyahatasistanim.data.repository
 
 import com.erendogan6.seyahatasistanim.data.local.TravelDao
 import com.erendogan6.seyahatasistanim.data.model.travel.TravelEntity
+import com.erendogan6.seyahatasistanim.data.model.weather.City
+import com.erendogan6.seyahatasistanim.data.remote.CityApiService
 
 class TravelRepository(
     private val travelDao: TravelDao,
+    private val cityApiService: CityApiService,
 ) {
     suspend fun saveTravelInfo(travelEntity: TravelEntity) {
         travelDao.insertTravelInfo(travelEntity)
     }
 
-    suspend fun getLastTravelInfo(): TravelEntity? = travelDao.getLastTravelInfo()
+    suspend fun getLastTravelInfo(): TravelEntity? {
+        val lastTravelInfo = travelDao.getLastTravelInfo()
+        return lastTravelInfo
+    }
 
     suspend fun deleteAllTravelInfo() {
         travelDao.deleteAllTravelInfo()
     }
+
+    suspend fun getCitySuggestions(query: String): List<City> = cityApiService.getCities(query = query)
 }
