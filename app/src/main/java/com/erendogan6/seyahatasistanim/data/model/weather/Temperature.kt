8ac71925aefe20ea3d0bf5f227bd20eb78c1c86c
erendogan6 <erendogan6@hotmail.com>
2024-08-23 00:50:33 +0300

    refactor(weather): remove unused fields from weather models

--- app/src/main/java/com/erendogan6/seyahatasistanim/data/model/weather/Temperature.kt
+++ app/src/main/java/com/erendogan6/seyahatasistanim/data/model/weather/Temperature.kt
@@ -4,9 +4,5 @@ import com.google.gson.annotations.SerializedName
 
 data class Temperature(
     @SerializedName("day") val day: Double,
-    @SerializedName("min") val min: Double,
-    @SerializedName("max") val max: Double,
     @SerializedName("night") val night: Double,
-    @SerializedName("eve") val eve: Double,
-    @SerializedName("morn") val morn: Double,
 )
--- app/src/main/java/com/erendogan6/seyahatasistanim/data/model/weather/WeatherForecast.kt
+++ app/src/main/java/com/erendogan6/seyahatasistanim/data/model/weather/WeatherForecast.kt
@@ -4,16 +4,6 @@ import com.google.gson.annotations.SerializedName
 
 data class WeatherForecast(
     @SerializedName("dt") val dateTime: Long,
-    @SerializedName("sunrise") val sunrise: Long,
-    @SerializedName("sunset") val sunset: Long,
     @SerializedName("temp") val temp: Temperature,
-    @SerializedName("feels_like") val feelsLike: FeelsLike,
-    @SerializedName("pressure") val pressure: Int,
-    @SerializedName("humidity") val humidity: Int,
     @SerializedName("weather") val weather: List<Weather>,
-    @SerializedName("speed") val speed: Double,
-    @SerializedName("deg") val degree: Int,
-    @SerializedName("clouds") val clouds: Int,
-    @SerializedName("rain") val rain: Double? = null,
-    @SerializedName("snow") val snow: Double? = null,
 )
